824c91ce92088917aeddcb5c572578ea1ba17308
trombiano1 <trombiano1@gmail.com>
2022-09-21 02:46:54 +0900

    new

--- test2.py
+++ test2.py
@@ -6,7 +6,7 @@ model_path = os.path.join('./exp/data/3D_CT.bin')
 ct3d = np.fromfile(model_path, dtype=np.float32)
 ctslices = np.reshape(ct3d, (-1, 128, 128))
 
-gt = ctslices[20, :, :]
+gt = ctslices[100, :, :]
 
-# plt.imshow(gt, interpolation='none', cmap='gray')
-# plt.savefig('test.png')
+plt.imshow(gt, interpolation='none', cmap='gray')
+plt.savefig('test.png')
